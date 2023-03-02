61d83cfc5a4755664fc3b8faa2977e4086413922
Vinicius Sempkoski <98129502+vinicius-sempkoski@users.noreply.github.com>
2023-03-02 10:14:29 -0300

    Add files via upload
    Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
    Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
    essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio2For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, c = 0, f = 0, num1;
+            Console.WriteLine("Digite um numero inteiro:");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                num1 = int.Parse(Console.ReadLine());
+                if (num1 >= 10 && num1 <= 20)
+                {
+                    f++;
+                }
+                else
+                {
+                    c++;
+                }
+            }
+            Console.WriteLine($"\n{f} in\n{c} out");
+        }
+    }
+}
