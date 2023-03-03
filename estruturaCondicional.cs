5ef6d4edf9c0c3058552e754cff93dbb5efc05c0
Vinicius Sempkoski <98129502+vinicius-sempkoski@users.noreply.github.com>
2023-03-03 07:53:44 -0300

    Add files via upload

---
+++ estruturaCondicional.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio1EstruturaCondicional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Digite um numero para verificar se é negativo ou não");
+            num = int.Parse(Console.ReadLine());
+
+            if(num < 0)
+            {
+                Console.WriteLine("NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine("NAO NEGATIVO");
+            }
+        }
+    }
+}
